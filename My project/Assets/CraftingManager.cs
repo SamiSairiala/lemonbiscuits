092a87c1ab8b38852d4e3efab922d6d14f3fbfcb
Sami Sairiala <sami.sairiala@tuni.fi>
2023-03-22 11:23:49 +0200

    Crafting System Works.

--- My project/Assets/CraftingManager.cs
+++ My project/Assets/CraftingManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CraftingManager : MonoBehaviour
 {
@@ -22,8 +23,47 @@ public class CraftingManager : MonoBehaviour
 	public Slot resultSlot;
 
 	public int Correct = 0;
-	
-	public void OnMouseDownItem(Item item)
+	private Item itemToReceive;
+
+	[SerializeField] private GameObject AcceptButton;
+	[SerializeField] private GameObject CraftingCanvas;
+
+	[Header("Items that can be used to craft here. Can Add more from script")]
+	public GameObject GameObject1;
+	public Item Item1;
+	public GameObject GameObject2; 
+	public Item Item2;
+
+
+
+
+    private void Update()
+    {
+		GameObject1.GetComponentInChildren<TextMeshProUGUI>().text = Item1.Amount.ToString();
+		GameObject2.GetComponentInChildren<TextMeshProUGUI>().text = Item2.Amount.ToString();
+		if (Item1.Amount == 0)
+		{
+			GameObject1.gameObject.SetActive(false);
+		}
+		if (Item2.Amount == 0)
+		{
+			GameObject2.gameObject.SetActive(false);
+		}
+		if (InventoryManager.Instance.Items.Contains(Item1))
+        {
+			GameObject1.gameObject.SetActive(true);
+			
+			
+        }
+        if (InventoryManager.Instance.Items.Contains(Item2))
+        {
+			GameObject2.gameObject.SetActive(true);
+			
+		}
+    }
+
+
+    public void OnMouseDownItem(Item item)
 	{
 		if(currentItem == null)
 		{
@@ -39,6 +79,7 @@ public class CraftingManager : MonoBehaviour
 					craftingSlots[i].item = currentItem;
 					itemList[i] = currentItem;
 					currentItem = null;
+					InventoryManager.Instance.Remove(itemList[i]);
 					CheckForRecipes();
 					break;
 				}
@@ -73,9 +114,77 @@ public class CraftingManager : MonoBehaviour
 				resultSlot.gameObject.SetActive(true);
 				resultSlot.GetComponent<Image>().sprite = recipeResults[i].Icon;
 				resultSlot.item = recipeResults[i];
-				InventoryManager.Instance.Add(recipeResults[i]);
+				itemToReceive = recipeResults[i];
+				AcceptButton.SetActive(true);
 				break;
 			}
 		}
+	}
+
+
+	public void RedoClose()
+    {
+		for(int i = 0; i < craftingSlots.Length; i++)
+        {
+			if(craftingSlots[i].item != null)
+            {
+				InventoryManager.Instance.Add(craftingSlots[i].item);
+				craftingSlots[i].gameObject.SetActive(false);
+				craftingSlots[i].GetComponent<Image>().sprite = default;
+				craftingSlots[i].item = default;
+			}
+			
+        }
+		
+		resultSlot.item = null;
+		resultSlot.gameObject.SetActive(false);
+		AcceptButton.SetActive(false);
+		ClearList();
+		List<Item> itemList = new List<Item>(4);
+
+	}
+
+	
+	public void Close() // Does the same as in RedoClose but also closes CraftingCanvas.
+    {
+		for (int i = 0; i < craftingSlots.Length; i++)
+		{
+			if (craftingSlots[i].item != null)
+			{
+				InventoryManager.Instance.Add(craftingSlots[i].item);
+				craftingSlots[i].gameObject.SetActive(false);
+				craftingSlots[i].GetComponent<Image>().sprite = default;
+				craftingSlots[i].item = default;
+			}
+
+		}
+
+		resultSlot.item = null;
+		resultSlot.gameObject.SetActive(false);
+		AcceptButton.SetActive(false);
+		ClearList();
+		List<Item> itemList = new List<Item>(4);
+		CraftingCanvas.SetActive(false);
+	}
+
+	void ClearList()
+    {
+		for(int i = 0; i < itemList.Capacity; i++)
+        {
+			itemList[i] = null;
+        }
+    }
+
+
+
+	public void AcceptCraft()
+    {
+		InventoryManager.Instance.Add(itemToReceive);
+		resultSlot.item = null;
+		resultSlot.gameObject.SetActive(false);
+		AcceptButton.SetActive(false);
+		CraftingCanvas.SetActive(false);
+		ClearList();
+		List<Item> itemList = new List<Item>(4);
 	}
 }
